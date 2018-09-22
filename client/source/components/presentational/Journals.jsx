c9e6193a2e1d8e3d24f16db096a34249ba94786b
vangny <vang.nathany@gmail.com>
2018-09-21 23:23:31 -0500

    Add static data into Journals which renders route but still needs to link to dashboard

--- client/source/components/presentational/Journals.jsx
+++ client/source/components/presentational/Journals.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 // import axios from 'axios';
 // import RouteContainer from './RouteContainer';
+import Route from './Route';
 
 class RouteHistory extends React.Component {
   constructor({ props /* routes */}) {
     super(props);
 
+<<<<<<< HEAD
     // this.state = {
     //   routes: '',
     // };
@@ -14,6 +16,37 @@ class RouteHistory extends React.Component {
     // this.addRoute = this.addRoute.bind(this);
     // this.handleDelete = this.handleDelete.bind(this);
     // this.handleUpsert = this.handleUpsert.bind(this);
+=======
+    this.state = {
+      routes: [{
+        name: 'Half Dome',
+        type: 'Featured Hike',
+        difficulty: 'black',
+        location: 'Yosemite Valley, California',
+        length: 14.7,
+        start: 'June 10, 10:05 AM',
+        end: 'June 10, 3:35 PM'
+      },
+      {
+        name: 'Devil\'s Drop',
+        type: 'Featured Hike',
+        difficulty: 'black',
+        location: 'Yosemite Valley, California',
+        length: 15.2,
+        start: 'June 5, 8:05 AM',
+        end: 'June 5, 2:21 PM'
+      },
+      {
+      name: 'Stairway To Heaven',
+      type: 'Featured Hike',
+      difficulty: 'black',
+      location: 'Yosemite Valley, California',
+      length: 12,
+      start: 'May 2, 9:30 AM',
+      end: 'May 2, 1:56 PM'
+      }],
+    };
+>>>>>>> Add static data into Journals which renders route but still needs to link to dashboard
   }
 
   // getRoutes() {
@@ -66,22 +99,25 @@ class RouteHistory extends React.Component {
   // }
 
   render() {
+    const { routes } = this.state;
+    // console.log(routes);
     return (
       <div className="routeHistory">
         <div className="routesHeader">
+<<<<<<< HEAD
           <h1>My Route Journal</h1>
           <button className="addRouteButton" type="button" onClick={this.addRoute}>Add New Route</button>
+=======
+          <h1>My Route History</h1>
+>>>>>>> Add static data into Journals which renders route but still needs to link to dashboard
         </div>
         <div className="routesContainer">
-          { /* routes.map(route => (
-            <RouteContainer
-              key={Math.random()}
-              route={route}
-              handleDelete={this.handleDelete}
-              handleUpsert={this.handleUpsert}
-          />))
-          This needs to be fixed once we receive real routes
-           */}
+          {routes.map(route => (
+            <div>
+              {}
+              <Route route={route}/>
+            </div>
+           ))}
         </div>
       </div>
     );
